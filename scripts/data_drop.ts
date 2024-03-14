84740e451608e51ade5059e42a79b2c4891c68d6
franz <franzns11@gmail.com>
2024-03-14 16:32:46 +0200

    change to specific block

--- scripts/data_drop.ts
+++ scripts/data_drop.ts
@@ -10,6 +10,8 @@ async function main() {
     const VALIDATOR_PICKER = '0xb09101eC9B3EC745129aC8e9c8a45F90B23c483C'
     const FTM_STAKING_PROXY = '0xB458BfC855ab504a8a327720FcEF98886065529b'
 
+    const blockNumber = 76563063
+
     const maxLockApr = 0.06
 
     const baseApr = 0.06 * 0.3
@@ -24,18 +26,19 @@ async function main() {
     console.log(`validatorpicker owner: ${validatorOwner}`)
 
     const ftmStaking = await ethers.getContractAt(FTMStaking.abi, FTM_STAKING_PROXY)
-    const maxVaultCount = await ftmStaking.maxVaultCount()
+    const maxVaultCount = await ftmStaking.maxVaultCount({ blockTag: blockNumber })
+    const exchangeRate = await ftmStaking.getExchangeRate({ blockTag: blockNumber })
     console.log(maxVaultCount.toString())
 
     let totalStaked = BigNumber.from('0')
 
     for (let i = 0; i < maxVaultCount; i++) {
-        const vaultAddress = await ftmStaking.getVault(i)
+        const vaultAddress = await ftmStaking.getVault(i, { blockTag: blockNumber })
         if (vaultAddress !== '0x0000000000000000000000000000000000000000') {
             const vault = await ethers.getContractAt(Vault.abi, vaultAddress)
-            const stakedAmount = await vault.currentStakeValue()
-            const validatorId = await vault.toValidatorID()
-            const owner = await vault.owner()
+            const stakedAmount = await vault.currentStakeValue({ blockTag: blockNumber })
+            const validatorId = await vault.toValidatorID({ blockTag: blockNumber })
+            const owner = await vault.owner({ blockTag: blockNumber })
             console.log(`Vault ID ${i} at addr ${vaultAddress} has ${stakedAmount} staked`)
             console.log(`Vault ID ${vaultAddress} delegated to validator ${validatorId}`)
             // console.log(`Vault ID ${vaultId} has owner ${owner}`)
@@ -57,16 +60,16 @@ async function main() {
 
     console.log(`Total staked amount is ${totalStaked} or ${ethers.utils.formatUnits(totalStaked, 18)} FTM`)
 
-    const maturedVaultCount = await ftmStaking.getMaturedVaultLength()
+    const maturedVaultCount = await ftmStaking.getMaturedVaultLength({ blockTag: blockNumber })
     let totalMatured = BigNumber.from('0')
 
     for (let i = 0; i < maturedVaultCount; i++) {
-        const vaultAddress = await ftmStaking.getMaturedVault(i)
+        const vaultAddress = await ftmStaking.getMaturedVault(i, { blockTag: blockNumber })
         if (vaultAddress !== '0x0000000000000000000000000000000000000000') {
             const vault = await ethers.getContractAt(Vault.abi, vaultAddress)
-            const stakedAmount = await vault.currentStakeValue()
-            const validatorId = await vault.toValidatorID()
-            const owner = await vault.owner()
+            const stakedAmount = await vault.currentStakeValue({ blockTag: blockNumber })
+            const validatorId = await vault.toValidatorID({ blockTag: blockNumber })
+            const owner = await vault.owner({ blockTag: blockNumber })
             console.log(`Vault ID ${i} at addr ${vaultAddress} has ${stakedAmount} staked`)
             // console.log(`Vault ID ${vaultId} delegated to validator ${validatorId}`)
             // console.log(`Vault ID ${vaultId} has owner ${owner}`)
@@ -88,7 +91,7 @@ async function main() {
 
     console.log(`Total matured staked amount is ${totalMatured} or ${ethers.utils.formatUnits(totalMatured, 18)} FTM`)
 
-    const ftmPoolAmount = await ftmStaking.getPoolBalance()
+    const ftmPoolAmount = await ftmStaking.getPoolBalance({ blockTag: blockNumber })
     console.log(`FTM pool balance is  ${ftmPoolAmount} or ${ethers.utils.formatUnits(ftmPoolAmount, 18)} FTM`)
 
     const totalFTM = totalStaked.add(totalMatured).add(ftmPoolAmount)
@@ -104,6 +107,7 @@ async function main() {
     console.log(`stakedPercentage: ${stakedPercentage}`)
     console.log(`maturedPercentage: ${maturedPercentage}`)
     console.log(`poolPercentage: ${poolPercentage}`)
+    console.log(`exchangeRate: ${exchangeRate}`)
 
     // const aprBase6 = (stakedPercentage * maxLockApr + maturedPercentage * baseApr) / 3
     // const aprBase5 = (stakedPercentage * maxLockApr * 0.85 + maturedPercentage * baseApr) / 3
